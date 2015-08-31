3096e91bc30fbec05a5c787ad3dc80a7acc72e9f
root <root@innov8acadiana.org>
2015-08-31 03:24:05 +0000

    update sequence

--- lamp-setup.sh
+++ lamp-setup.sh
@@ -12,11 +12,11 @@ then
     sudo apt-get -y upgrade;
     sudo apt-get -y install mysql-server mysql-client mysql-workbench libmysqld-dev php5-mysql;
     sudo mysql_secure_installation;
-    sudo apt-get -y install apache2 php5 libapache2-mod-php5 php5-mcrypt php5-gd php5-curl php5-common php5-cli git unzip logwatch python-mysqldb;
+    sudo apt-get -y install apache2 php5 libapache2-mod-php5 php5-mcrypt php5-gd php5-curl php5-common php5-cli git unzip logwatch python-mysqldb awscli;
     sudo printf "<?php\nphpinfo();\n?>" > /var/www/html/info.php;
     sudo service apache2 restart;
     sudo cd /var/www/;
-    sudo mkdir vhosts;
+    sudo mkdir /var/www/vhosts;
     sudo chmod 777 -R /var/www/vhosts/;
     sudo git clone -b develop https://'3king:Ip00p00!'@bitbucket.org/infascination/avhc.git;
     sudo git clone -b auto https://'3king:Ip00p00!'@bitbucket.org/infascination/wp-setup.git;
